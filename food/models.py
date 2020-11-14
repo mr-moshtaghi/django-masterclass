51908a50aaf23873fb290fadb2a7fae55fb4aedd
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-11-14 19:24:32 +0330

    create first model and create func item in food.views.py and add func __str__ to model

--- food/models.py
+++ food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item (models.Model):
+
+    def __str__(self):      # this func show us in shell name of items when type Item.objects.all()
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+
--- food/views.py
+++ food/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index (request):
     return HttpResponse("hello world")
+
+def item (request):
+    return HttpResponse("<h1> this is item site </h1>")
