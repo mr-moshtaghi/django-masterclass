cbb12d0590be3c82a42853372292877a60b1a158
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-11-14 22:23:33 +0330

    create detail view

--- food/urls.py
+++ food/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # /food/1/
+    path("<int:item_id>/" , views.detail , name="detail"),
     path('item/', views.item, name='item'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Item
 
 
 def index (request):
     item_list = Item.objects.all()
-    tempalte = loader.get_template("food.html")
-    context ={
-
+    context = {
+        'item_list' : item_list,
     }
-    return HttpResponse(tempalte.render(context , request))
-    
-    # return HttpResponse("hello world") # example of HttpResponse
+    return render(request , "food.html" , context)
+
+
+    # example of HttpResponse
+    #return HttpResponse(tempalte.render(context , request)) 
+    # return HttpResponse("hello world") 
 
 def item (request):
     return HttpResponse("<h1> this is item site </h1>")
+
+
+def detail(request , item_id):    # item_id ---------> <int:item_id> in path("<int:item_id>/" , views.detail , name="detail")
+    item = Item.objects.get(pk = item_id)
+    context = {
+        "item" : item,
+    }
+    return render(request , "detail.html" , context) 
