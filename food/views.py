ab609f235d903ae838d190dfdbdc281ca78ea60f
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-11-14 20:37:27 +0330

    use advance HttpResponse

--- food/views.py
+++ food/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Item
 
 
 def index (request):
-    return HttpResponse("hello world")
+    item_list = Item.objects.all()
+    tempalte = loader.get_template("food.html")
+    context ={
+
+    }
+    return HttpResponse(tempalte.render(context , request))
+    
+    # return HttpResponse("hello world") # example of HttpResponse
 
 def item (request):
     return HttpResponse("<h1> this is item site </h1>")
