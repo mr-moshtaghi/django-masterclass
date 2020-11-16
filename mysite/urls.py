02ea2d2438545c65ad185878d01f50af22301e3a
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-11-16 22:06:54 +0330

    add login to project

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.contrib.auth import views
 
 from users import views as user_views
 
@@ -22,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include("food.urls")),
     path('register/', user_views.register, name='register'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
